a1da8023dbb2ecb7c86a613a0ec8e608137a5189
abrarshah00 <abrarshah00@gmail.com>
2023-07-16 11:10:32 +0530

    :sparkles: updated pre commits hooks with linting for staged files only

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <>Hello World</>
+    <h1>Hello World</h1>
     <!-- <NuxtWelcome /> -->
   </div>
 </template>
+<script setup lang="ts">
+function greet(name: string) {
+  console.log(`Hello, ${name}!`);
+}
+
+const age = '25'; // This should be a string
+
+greet(age);
+</script>
